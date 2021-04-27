631ca8bfd920d64ebec70f2f6661f43d23f5b40c
crispy <abdurtaalib@gmail.com>
2021-04-27 21:36:32 +0000

    e

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from visudata!');
+		vscode.window.showInformationMessage('Hello World from VisuData!');
 	});
 
 	context.subscriptions.push(disposable);
